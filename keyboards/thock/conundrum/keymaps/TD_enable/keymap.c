25a31f76a0a6247e6264368aa9fb2392718f614c
axolotl-being <nora.and.the.moon@gmail.com>
2024-06-15 13:03:20 -0500

    adding my personal keymaps

--- keyboards/thock/conundrum/keymaps/TD_enable/keymap.c
+++ keyboards/thock/conundrum/keymaps/TD_enable/keymap.c
@@ -3,23 +3,33 @@
 #include "quantum.h"
 
 enum layers {
-  _QWERTY,
   _COLEMAK,
-  _DVORAK,
+  _QWERTY,
+  _GAMER,
   _LOWER,
   _RAISE,
   _ADJUST
 };
 
 enum keycodes {
-  QWERTY = SAFE_RANGE,
-  COLEMAK,
-  DVORAK
+  COLEMAK = SAFE_RANGE,
+  QWERTY,
+  GAMER
 };
 
+// Tap Dance declarations
+enum {
+    TD_1_TAB
+};
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for 1, twice for Tab
+    [TD_1_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_TAB),
+};
+
+#define LOWER TT(_LOWER)
+#define RAISE TT(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -49,17 +59,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * |cp/pst| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_grid(
-    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, LT(0,KC_BSPC),
     KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_GRV,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    LT(0,KC_NO),  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
-/* Dvorak
+/* dvorak
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -70,11 +80,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |Qwerty| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[_DVORAK] = LAYOUT_grid(
-    KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH,
-    KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT ,
-    QWERTY,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+[_GAMER] = LAYOUT_grid(
+    TD(TD_1_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    LT(0,KC_2),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    LSFT_T(KC_3), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    LCTL_T(KC_4), KC_GRV, KC_LALT, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -89,10 +99,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+KC_TRNS, KC_F12, KC_F7, KC_F8, KC_F9, KC_PLUS, KC_RPRN, KC_AMPR, KC_ASTR, KC_TILD, KC_RCBR, KC_TRNS, 
+KC_NUMLOCK, KC_F11, KC_F4, KC_F5, KC_F6, KC_UNDS, KC_LPRN, KC_DLR, KC_PERC, KC_CIRC, KC_LCBR, KC_DEL, 
+KC_TRNS, KC_F10, KC_F1, KC_F2, KC_F3, KC_TRNS, KC_PIPE, KC_EXLM, KC_AT, KC_HASH, KC_TRNS, KC_BSPC, 
+KC_TRNS, KC_LCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Raise
@@ -107,15 +117,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+KC_LNUM, KC_TRNS, KC_P7, KC_P8, KC_P9, KC_EQL, KC_RBRC, KC_F7, KC_F8, KC_F9, KC_F12, KC_TRNS, 
+KC_CAPS, KC_DEL, KC_P4, KC_P5, KC_P6, KC_PMNS, KC_LBRC, KC_F4, KC_F5, KC_F6, KC_F11, KC_GRV, 
+KC_LSFT, KC_BSPC, KC_P1, KC_P2, KC_P3, KC_TRNS, KC_BSLS, KC_F1, KC_F2, KC_F3, KC_F10, KC_RSFT, 
+KC_TRNS, KC_LCTL, KC_P0, KC_P0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_PGDN, KC_PGUP, KC_HOME
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      |Qwerty|Colemk|Dvorak|      |      |      |      |      |      |      | Reset|
+ * |      |Colemk|QWERTY|GAMER|      |      |      |      |      |      |      | Reset|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -125,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_grid(
-    _______, QWERTY,  COLEMAK, DVORAK,  _______, _______, _______, _______, _______,  _______, _______, RESET,
+    _______, COLEMAK,  QWERTY,   GAMER,  _______, _______, _______, _______, _______,  _______, _______, RESET,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
@@ -151,12 +161,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case DVORAK:
+    case GAMER:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_DVORAK);
+        set_single_persistent_default_layer(_GAMER);
       }
       return false;
       break;
+	//handle custom mod tap functions in the GAMER layer
+	
+	/* unused
+	case LT(0,KC_1):
+      if (!record->tap.count && record->event.pressed) {
+          tap_code16(KC_TAB); // Intercept hold function to send TAB
+          return false;
+      }
+      return true;             // Return true for normal processing of tap keycode
+	*/
+	
+    case LT(0,KC_2):
+      if (!record->tap.count && record->event.pressed) {
+          tap_code16(KC_ESC); // Intercept hold function to send ESC
+          return false;
+      }
+      return true;             // Return true for normal processing of tap keycode
+	//tap backspace, hold is ctrl+bkspc
+	case LT(0,KC_BSPC):
+      if (!record->tap.count && record->event.pressed) {
+          tap_code16(C(KC_BSPC)); // Intercept hold function to send CTRL-BKSPC
+          return false;
+      }
+      return true;             // Return true for normal processing of tap keycode
+	//handle copy and paste on a single key
+	case LT(0,KC_NO):
+      if (record->tap.count && record->event.pressed) {
+          tap_code16(C(KC_C)); // Intercept tap function to send Ctrl-C
+      } else if (record->event.pressed) {
+          tap_code16(C(KC_V)); // Intercept hold function to send Ctrl-V
+      }
+      return false;
   }
   return true;
 }
--- keyboards/thock/conundrum/rules.mk
+++ keyboards/thock/conundrum/rules.mk
@@ -21,13 +21,14 @@ CUSTOM_MATRIX = yes
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
-EXTRAKEY_ENABLE = no # yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = yes         # Console for debug(+400)
+EXTRAKEY_ENABLE = no       # Audio control and System control(+450)
+CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no # yes           # USB Nkey Rollover
+NKRO_ENABLE =  yes           # USB Nkey Rollover
 MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
 UNICODE_ENABLE = no         # Unicode
 VIRTSER_ENABLE = no         # USB Serial Driver
 RAW_ENABLE = no             # Raw device
 AUTO_SHIFT_ENABLE = no      # Auto Shift
 TAP_DANCE_ENABLE = no
+NO_USB_STARTUP_CHECK = yes
